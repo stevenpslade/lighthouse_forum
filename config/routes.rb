f02996e9b243b46a727fe07b077f22d9e3735437
stevenpslade <stevenpslade@gmail.com>
2015-10-26 04:14:38 +0000

    added route to posts and displays properly

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 LighthouseForum::Application.routes.draw do
+
+  get 'posts', to: 'posts#index'
+
+  # [tons of helpful comments]
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +58,3 @@ LighthouseForum::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
