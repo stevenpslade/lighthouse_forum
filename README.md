fb9ca49143d0fc96db891318e5daf026be476bdb
stevenpslade <stevenpslade@gmail.com>
2015-10-26 04:03:51 +0000

    all created rails files for forum

---
+++ README.md
@@ -0,0 +1,8 @@
+== README
+
+Rails 4.0.0
+
+Lighthouse Forum
+Welcome to our introduction to Rails!
+
+We're going to build a simple open forum app that allows users to Create, Read, Update, and Destroy posts. Those words should sound familiar. CRUD is a central tenet to building relational database-backed applications (the kind Rails is great at!), so this tutorial should provide some great building blocks to use in the web apps you'll be building for the next few weeks and months and years. Very exciting.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LighthouseForum::Application.config.secret_key_base = '91fb8974f8a7f94f103f4ddf1b2c2e8ce280fb7b3ae1fd24c8aa9ab33fb999e2e0bbf375645d94e1cbb4d06a456751f7d608ec7d0ca8e4d0f81a8954d928fb64'
